58d87965771e1273e5fbeb194aca1207ded3f195
hell-knight <hell_knight@tutamail.com>
2026-04-12 10:36:01 -0700

    Unbreackable and smooth desroyable blocks added

--- ArkanoidGame/Block.cpp
+++ ArkanoidGame/Block.cpp
@@ -42,4 +42,54 @@ namespace ArkanoidGame
 	{
 		return hitCount <= 0;
 	}
+
+	void SmoothDestroyableBlock::OnHit()
+	{
+		StartTimer(BREAK_DELAY);
+	}
+
+	SmoothDestroyableBlock::SmoothDestroyableBlock(const sf::Vector2f& position, const sf::Color& color)
+		: Block(position, color)
+		, color(color)
+	{
+	}
+
+	void SmoothDestroyableBlock::Update(float timeDelta)
+	{
+		UpdateTimer(timeDelta);
+	}
+
+	bool SmoothDestroyableBlock::GetCollision(std::shared_ptr<Collidable> collidableObject) const
+	{
+		if (isTimerStarted_)
+		{
+			return false;
+		}
+
+		auto gameObject = std::dynamic_pointer_cast<GameObject>(collidableObject);
+		assert(gameObject);
+		sf::Rect rect = gameObject->GetRect();
+		rect.width *= 1.1f;
+		return GetRect().intersects(gameObject->GetRect());
+	}
+
+	void SmoothDestroyableBlock::FinalAction()
+	{
+		--hitCount;
+	}
+
+	void SmoothDestroyableBlock::EachTickAction(float deltaTime)
+	{
+		color.a = (sf::Uint8)(255 * currentTime_ / destroyTime_);
+		sprite.setColor(color);
+	}
+
+	UnbreackableBlock::UnbreackableBlock(const sf::Vector2f& position)
+		: Block(position, sf::Color::Color(105, 105, 105))
+	{
+	}
+
+	void UnbreackableBlock::OnHit()
+	{
+	}
 }
--- ArkanoidGame/Block.h
+++ ArkanoidGame/Block.h
@@ -2,6 +2,7 @@
 #include "GameObject.h"
 #include "Ball.h"
 #include "Collidable.h"
+#include "IDelayedAction.h"
 
 namespace ArkanoidGame
 {
@@ -16,5 +17,32 @@ namespace ArkanoidGame
 		bool GetCollision(std::shared_ptr<Collidable> collidableObject) const override;
 		void Update(float timeDelta) override;
 		bool IsBroken();
+	};
+
+	class SmoothDestroyableBlock : public Block, public IDelayedAction
+	{
+	protected:
+		void OnHit() override;
+		sf::Color color;
+	public:
+		SmoothDestroyableBlock(const sf::Vector2f& position, const sf::Color& color = sf::Color::Green);
+		~SmoothDestroyableBlock() = default;
+		void Update(float timeDelta) override;
+
+		bool GetCollision(std::shared_ptr<Collidable> collidableObject) const override;
+		void FinalAction() override;
+		void EachTickAction(float deltaTime) override;
+	};
+
+	class UnbreackableBlock : public Block
+	{
+	public:
+		UnbreackableBlock(const sf::Vector2f& position);
+		void OnHit() override;
+		void Update(float)
+		{
+			int i = 0;
+			++i;
+		}
 	};
 }
